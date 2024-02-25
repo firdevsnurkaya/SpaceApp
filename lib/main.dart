843a42fd79292fc05968ac5f66b2f3ca43da23a3
birsendalmis <birsen.dlms.80@gmail.com>
2024-02-25 21:36:31 +0300

    debug yazısı kaldırıldı

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'All About Space',
       theme: ThemeData.dark().copyWith(
         primaryColor: Color.fromARGB(255, 74, 72, 72),
