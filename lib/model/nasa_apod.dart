d7741ce729122ac50663f8a62ce72a1266b8c874
birsendalmis <birsen.dlms.80@gmail.com>
2024-02-28 21:07:42 +0300

    apı değişikliği

--- lib/model/nasa_apod.dart
+++ lib/model/nasa_apod.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 Future<Map<String, dynamic>?> fetchAPOD(DateTime date) async {
   // NASA API anahtarını buraya ekleyin (opsiyonel, ancak bazı API'ler için gerekebilir)
   const String apiKey =
-      'TlQM4usYPfMG8tNtXS3ThLdlExNyYnEbDvcWywno'; // NASA API anahtarını buraya ekleyin
+      '6jNd3MjcKyjnWixijJiHIqHeGe7wELzPG1LcG1Ve'; // NASA API anahtarını buraya ekleyin
 
   final formattedDate = '${date.year}-${date.month}-${date.day}';
 
